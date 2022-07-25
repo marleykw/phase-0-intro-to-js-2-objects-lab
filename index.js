24fab56e41bdd8386e05c123e9beddc40e6c8a71
Marley Watson <marleykw17@gmail.com>
2022-07-25 09:36:38 -0400

    commit tree 3b106a1d52d531be7cc70f84f50c50ba012825ba

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+const employee = {
+    name : "Marley",
+    streetAddress : "61 Lispendard"
+}; 
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   return {
+       ...employee,
+       [key] : value
+   };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {
+        ...employee
+    };
+    delete newEmployee.name;
+    //employee.name;
+    console.log(employee.name);
+    console.log(newEmployee.name);
+    return newEmployee;
+    return employee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    const newEmployee = {...employee};
+    console.log(employee);
+    console.log(newEmployee);
+    return employee;
+    return newEmployee;
+}
